ceacde4426325b7b59e8ae43a13d55d406f89f1d
NickWalla1146 <WALLANR@dukes.jmu.edu>
2024-04-23 12:23:55 -0400

    thrying to get models from form

--- scannergallery/users/views.py
+++ scannergallery/users/views.py
@@ -110,6 +110,7 @@ def imageCreate(request):
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
+            form.save()
             return HttpResponseRedirect("/gallery")
 
     # if a GET (or any other method) we'll create a blank form
